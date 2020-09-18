41b86d1affb851d798f286a9ee9b0addc83a82c5
bucketYang <515971947@qq.com>
2020-09-18 16:33:11 +0800

    darkswap update v0.3

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -27,8 +27,8 @@ const Farms: React.FC = () => {
             <Route exact path={path}>
               <PageHeader
                 icon={<img src={chef} height="0" />}
-                subtitle="Earn SUSHI tokens by staking Uniswap V2 LP Tokens."
-                title="Select Your Favorite Dishes"
+                subtitle="Earn DARK by Staking LP Tokens."
+                title="Own Your Darkness"
               />
               <FarmCards />
             </Route>
