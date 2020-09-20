a83e71a1e85a7c4c89bc10fab007d4c489903ead
bucketYang <515971947@qq.com>
2020-09-21 02:20:23 +0800

    Updates

---
+++ precache-manifest.39a85e77a3f9ecc946870a8ad24b1945.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a7df1ff1c96e7d1e04d12946e970b4",
+    "url": "/Darkswap-frontend/index.html"
+  },
+  {
+    "revision": "8e3f7e25982c0cc6dbf1",
+    "url": "/Darkswap-frontend/static/css/main.5f59d50f.chunk.css"
+  },
+  {
+    "revision": "65877c0cbd2055995201",
+    "url": "/Darkswap-frontend/static/js/0.a0d76849.chunk.js"
+  },
+  {
+    "revision": "2e158a03638b2beee253",
+    "url": "/Darkswap-frontend/static/js/10.918b78db.chunk.js"
+  },
+  {
+    "revision": "3ca4e24fb2ec65b84915",
+    "url": "/Darkswap-frontend/static/js/11.b71cce41.chunk.js"
+  },
+  {
+    "revision": "1a6738077ed7489587d5",
+    "url": "/Darkswap-frontend/static/js/3.2860c4b3.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "/Darkswap-frontend/static/js/3.2860c4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c39f1eb40b0cbc3d0f4",
+    "url": "/Darkswap-frontend/static/js/4.e9e552a8.chunk.js"
+  },
+  {
+    "revision": "771b92b809693b52512c",
+    "url": "/Darkswap-frontend/static/js/5.a5e7bfcb.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/Darkswap-frontend/static/js/5.a5e7bfcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd5358b7c8a20962944",
+    "url": "/Darkswap-frontend/static/js/6.430d4c58.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/Darkswap-frontend/static/js/6.430d4c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf63c49a833acf9d39a",
+    "url": "/Darkswap-frontend/static/js/7.b087b47d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/Darkswap-frontend/static/js/7.b087b47d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea49049725fe901ffb64",
+    "url": "/Darkswap-frontend/static/js/8.20339736.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/Darkswap-frontend/static/js/8.20339736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5832c01b5f2870d7cc1",
+    "url": "/Darkswap-frontend/static/js/9.bfd2f273.chunk.js"
+  },
+  {
+    "revision": "8e3f7e25982c0cc6dbf1",
+    "url": "/Darkswap-frontend/static/js/main.7b60fe5e.chunk.js"
+  },
+  {
+    "revision": "ab0073aec2e36fa52687",
+    "url": "/Darkswap-frontend/static/js/runtime-main.299e0459.js"
+  },
+  {
+    "revision": "4c8215f485bbf6befcce40356f5fb6f2",
+    "url": "/Darkswap-frontend/static/media/White.4c8215f4.svg"
+  },
+  {
+    "revision": "3ada91a8139ed07c4115ee3c3fe5cff6",
+    "url": "/Darkswap-frontend/static/media/bg.3ada91a8.jpg"
+  },
+  {
+    "revision": "975e7410331f7a7ca23d818c33749fa5",
+    "url": "/Darkswap-frontend/static/media/block_bg.975e7410.jpg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/Darkswap-frontend/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "d1f736120a29462726fb25a8e88cffba",
+    "url": "/Darkswap-frontend/static/media/wallet-connect.d1f73612.svg"
+  }
+]);
